db9b8de2375b09c87af427a33307246c04f7e01e
lgorse <lgorse@mac.com>
2025-09-20 22:45:55 -0700

    added Cloudflare tunnel - mainly an installation

--- squash.py
+++ squash.py
@@ -118,6 +118,8 @@ def book_courts():
         return jsonify(response), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+    finally:
+        driver.quit()
 
 
 def main():
